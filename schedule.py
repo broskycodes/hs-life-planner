bd44d2d1044c8830b49d945e5077fcf58e4b9f9f
broskycodes <shravansaranyan@gmail.com>
2025-05-27 10:42:05 -0700

    Finished DailySchedule add_event

--- schedule.py
+++ schedule.py
@@ -36,14 +36,33 @@ class DailySchedule():
     def current_event(self, current_event):
         self._current_event = current_event
 
-    def add_event(self, event, time):
-        # Updates current event when time is reached
-        def update_current_event(self, event):
-            self.current_event = event
-        # Add feature to check if event is already occuring? Then provide a warning?
-        schedule.every().day.at(time).do(update_current_event(self, event))
+    @property
+    def daily_schedule(self):
+        return self._daily_schedule
 
-# Updates a given schedule with a specific item given a fixed time
+    @daily_schedule.setter
+    def daily_schedule(self, daily_schedule):
+        self._daily_schedule = daily_schedule
+
+    # Adds given events to daily schedule
+    # Assumes that events and times are correspondant
+    def add_event(self, events, times):
+        # Updates current event when time is reached, and adds event to schedule
+        for i in range(len(events)):
+            def update_current_event(self, event):
+                self.current_event = event
+            # Add feature to check if event is already occuring? Then provide a warning?
+            schedule.every().day.at(times[i]).do(
+                update_current_event(self, events[i]))
+        # Updates daily schedule with events
+        self.daily_schedule = time_order_update(self.daily_schedule, events)
+
+
+class WeeklySchedule():
+    def __init__():
+        pass
+
+# Updates and sorts a given schedule with each item if each item has a fixed time
 
 
 def time_order_update(schedule, items):
@@ -52,6 +71,5 @@ def time_order_update(schedule, items):
         if item.output_time_in_seconds() == -1:
             continue
         schedule.update({item.daily_title_display(): item})
-        {k: v for k, v in sorted(
+    return {k: v for k, v in sorted(
             schedule.values(), key=lambda item: item.output_time_in_seconds())}
-    return schedule
