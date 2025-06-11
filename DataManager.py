d279401507430f8953ed8ff99f3612bfd1328c7d
broskycodes <shravansaranyan@gmail.com>
2025-06-10 22:21:19 -0700

    Finished uploading/downloading logic from JSONs

--- DataManager.py
+++ DataManager.py
@@ -1,21 +1,22 @@
 import json
 import os
-from typing import Type, List
 
 
 class DataManager:
     def __init__(self, folder):
         self.folder = folder
-        os.makedirs(folder, exist_ok=True)
+        if not (os.path.exists(folder)):
+            os.makedirs(folder, exist_ok=True)
 
     def save(self, filename, data):
-        with open(os.path.join(self.folder, filename), "w") as file:
+        file_path = os.path.join(self.folder, filename)
+        with open(file_path, "w") as file:
             json.dump(data, file, indent=4)
 
-    def load(self, filename):
+    def load(self, filename, default=None):
         path = os.path.join(self.folder, filename)
         if not os.path.exists(path):
-            self.save(self, filename, None)
+            self.save(filename, default)
         with open(path) as file:
             return json.load(file)
 
@@ -24,4 +25,4 @@ class DataManager:
         for file in os.listdir(self.folder):
             if file.endswith(".json"):
                 data[file.replace(".json", "")] = self.load(file)
-        return data
+        return data.items()
